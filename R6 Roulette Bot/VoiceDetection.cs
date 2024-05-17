af816ad3c3d4a9f8fa9d90628be47cb6903e0d8e
bourk94 <alexandrebourque94@gmail.com>
2024-05-17 14:52:05 -0400

    Code cleanup

--- R6 Roulette Bot/VoiceDetection.cs
+++ R6 Roulette Bot/VoiceDetection.cs
@@ -1,8 +1,13 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.VoiceNext;
+using DSharpPlus.VoiceNext.Codec;
 using DSharpPlus.VoiceNext.EventArgs;
+using Microsoft.VisualBasic;
 using Pv;
 using R6_Roulette_Bot.Commands;
+using Concentus.Structs;
+using NAudio.Wave;
+using Concentus;
 
 
 namespace R6_Roulette_Bot
@@ -11,7 +16,7 @@ namespace R6_Roulette_Bot
     {
 
         public CommandContext? commandContext { get; set; }
-        private CommandRoulette commandRoulette;
+        private readonly CommandRoulette commandRoulette;
         private static Porcupine? porcupine;
 
         public VoiceDetection(CommandRoulette commandRoulette)
@@ -40,50 +45,29 @@ namespace R6_Roulette_Bot
 
         public async Task ReceiveHandler(VoiceNextConnection _, VoiceReceiveEventArgs args)
         {
+            short[] frame = new short[porcupine.FrameLength];
 
             byte[] pcmData = args.PcmData.ToArray();
 
-            // Convertir byte[] en short[]
             short[] audioFrame = new short[pcmData.Length / sizeof(short)];
             Buffer.BlockCopy(pcmData, 0, audioFrame, 0, pcmData.Length);
 
             if (audioFrame.Length > porcupine.FrameLength)
             {
-                int frameSize = porcupine.FrameLength;
                 int offset = audioFrame.Length - porcupine.FrameLength;
 
-                for (int i = 0; i + frameSize <= audioFrame.Length; i += offset)
+                for (int i = 0; i + porcupine.FrameLength <= audioFrame.Length; i += offset)
                 {
-                    short[] frame = new short[frameSize];
-                    Array.Copy(audioFrame, i, frame, 0, frameSize);
-
-                    // Passer la trame à Porcupine
-                    if (porcupine != null)
-                    {
-                        int keywordIndex = porcupine.Process(frame);
-                        if (keywordIndex >= 0)
-                        {
-                            await commandRoulette.RouletteStrat(GetCommandContext());
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Porcupine n'est pas initialisé");
-                    }
+                    Array.Copy(audioFrame, i, frame, 0, porcupine.FrameLength);
                 }
             }
             else
             {
-                // Traiter les données audio par trames
                 for (int i = 0; i < audioFrame.Length; i += porcupine.FrameLength)
                 {
-                    short[] frame = new short[porcupine.FrameLength];
-
-                    // Copier les données audio dans la trame
                     int remaining = audioFrame.Length - i;
                     Array.Copy(audioFrame, i, frame, 0, Math.Min(remaining, porcupine.FrameLength));
 
-                    // Si nous n'avons pas assez de données audio pour une trame complète, compléter avec des zéros
                     if (remaining < porcupine.FrameLength)
                     {
                         for (int j = remaining; j < porcupine.FrameLength; j++)
@@ -91,33 +75,23 @@ namespace R6_Roulette_Bot
                             frame[j] = 0;
                         }
                     }
-
-                    // Passer la trame à Porcupine
-                    if (porcupine != null)
-                    {
-                        int keywordIndex = porcupine.Process(frame);
-                        if (keywordIndex >= 0)
-                        {
-                            await commandRoulette.RouletteStrat(GetCommandContext());
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Porcupine n'est pas initialisé");
-                    }
                 }
             }
+
+            if (porcupine != null)
+            {
+                int keywordIndex = porcupine.Process(frame);
+                if (keywordIndex >= 0)
+                {
+                    await commandRoulette.RouletteStrat(commandContext);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Porcupine n'est pas initialisé");
+            }
+
             await Task.Yield();
-        }
-
-        public void SetCommandContext(CommandContext ctx)
-        {
-            commandContext = ctx;
-        }
-
-        private CommandContext GetCommandContext()
-        {
-            return commandContext;
         }
     }
 }
